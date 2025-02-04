c92eb3cb2ec6527199f3f4ee3c204b64cd334588
Tim Billiet <timbilliet@gmx.com>
2025-02-04 13:49:27 +0100

    version update

--- src/main/java/co/crystaldev/client/gui/screens/override/ScreenMainMenu.java
+++ src/main/java/co/crystaldev/client/gui/screens/override/ScreenMainMenu.java
@@ -46,7 +46,7 @@ public class ScreenMainMenu extends ScreenPanorama implements GuiYesNoCallback {
 
     private static final int aMods = Loader.instance().getActiveModList().size();
 
-    private static final String CC_VERSION = String.format("%s v%s", "Timmeke_ Crystal Client", "1.1.1");
+    private static final String CC_VERSION = String.format("%s v%s", "Timmeke_ Crystal Client", "1.1.2");
 
     private static final String MC_COPYRIGHT = "Copyright Mojang Studios. Do not distribute!";
 

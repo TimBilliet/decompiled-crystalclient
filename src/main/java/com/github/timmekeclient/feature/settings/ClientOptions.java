4b0c06365bd9db0a0b8860f7efac58d8764a2656
Tim Billiet <timbilliet@gmx.com>
2025-04-12 23:59:49 +0200

    change version to 1.4.0

--- src/main/java/com/github/timmekeclient/feature/settings/ClientOptions.java
+++ src/main/java/com/github/timmekeclient/feature/settings/ClientOptions.java
@@ -222,7 +222,7 @@ public class ClientOptions extends Module implements IRegistrable {
         if (this.dontOverrideDisplayTitle) {
             Display.setTitle("Minecraft " + Client.getMinecraftVersion().getVersionString());
         } else {
-            Display.setTitle(String.format("Minecraft %s | %s (v%s)", Client.getMinecraftVersion().getVersionString(),"Timmeke_ Client", "1.3.2"));
+            Display.setTitle(String.format("Minecraft %s | %s (v%s)", Client.getMinecraftVersion().getVersionString(),"Timmeke_ Client", "1.4.0"));
         }
     }
 

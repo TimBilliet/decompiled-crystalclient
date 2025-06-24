1bdbc1550fab5f7c23be6bec5aa2bd1a8f5ffa87
Tim Billiet <timbilliet@gmx.com>
2025-06-25 01:32:30 +0200

    v1.5.0 release

--- src/main/java/com/github/timmekeclient/ForgeMod.java
+++ src/main/java/com/github/timmekeclient/ForgeMod.java
@@ -3,7 +3,7 @@ package com.github.timmekeclient;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "timmekeclient", name = "Timmeke_ Client", version = "1.4.0", clientSideOnly = true, acceptedMinecraftVersions = "[1.8.9]")
+@Mod(modid = "timmekeclient", name = "Timmeke_ Client", version = "1.5.0", clientSideOnly = true, acceptedMinecraftVersions = "[1.8.9]")
 public class ForgeMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/timmekeclient/gui/screens/override/ScreenMainMenu.java
+++ src/main/java/com/github/timmekeclient/gui/screens/override/ScreenMainMenu.java
@@ -43,7 +43,7 @@ public class ScreenMainMenu extends ScreenPanorama implements GuiYesNoCallback {
 
     private static final int aMods = Loader.instance().getActiveModList().size();
 
-    private static final String CC_VERSION = String.format("%s v%s", "Timmeke_ Client", "1.4.0");
+    private static final String CC_VERSION = String.format("%s v%s", "Timmeke_ Client", "1.5.0");
 
     private static final String MC_COPYRIGHT = "Copyright Mojang Studios. Do not distribute!";
 
